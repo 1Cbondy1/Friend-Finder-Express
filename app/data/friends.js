8d8b9a61b4b71ffa7d4bb38888bfcdbbdcd325fd
1Cbondy1 <claytonbondy@gmail.com>
2018-12-02 00:15:57 -0600

    built html for home page and added custom logo

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var friends = 
+    [{
+        name: "Donald",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjugfy5noDfAhVGeawKHWD7AzYQjRx6BAgBEAU&url=https%3A%2F%2Fwww.forbes.com%2Fprofile%2Fdonald-glover%2F&psig=AOvVaw2IYby8AkmkEqB0nsbDx7J4&ust=1543808840854112",
+        scores: [1, 2, 3, 4, 5, 6, 7, 8]
+    },
+    {
+        name: "Donald",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjugfy5noDfAhVGeawKHWD7AzYQjRx6BAgBEAU&url=https%3A%2F%2Fwww.forbes.com%2Fprofile%2Fdonald-glover%2F&psig=AOvVaw2IYby8AkmkEqB0nsbDx7J4&ust=1543808840854112",
+        scores: [1, 2, 3, 4, 5, 6, 7, 8]
+    },
+    {
+        name: "Donald",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjugfy5noDfAhVGeawKHWD7AzYQjRx6BAgBEAU&url=https%3A%2F%2Fwww.forbes.com%2Fprofile%2Fdonald-glover%2F&psig=AOvVaw2IYby8AkmkEqB0nsbDx7J4&ust=1543808840854112",
+        scores: [1, 2, 3, 4, 5, 6, 7, 8]
+    },
+    {
+        name: "Donald",
+        photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjugfy5noDfAhVGeawKHWD7AzYQjRx6BAgBEAU&url=https%3A%2F%2Fwww.forbes.com%2Fprofile%2Fdonald-glover%2F&psig=AOvVaw2IYby8AkmkEqB0nsbDx7J4&ust=1543808840854112",
+        scores: [1, 2, 3, 4, 5, 6, 7, 8]
+    }]   
