8f9d26615b75144fe3ba91a1446f71e4165831ac
1Cbondy1 <claytonbondy@gmail.com>
2018-12-02 15:44:33 -0600

    built server.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,20 +2,20 @@ var friends =
     [{
         name: "Donald",
         photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjugfy5noDfAhVGeawKHWD7AzYQjRx6BAgBEAU&url=https%3A%2F%2Fwww.forbes.com%2Fprofile%2Fdonald-glover%2F&psig=AOvVaw2IYby8AkmkEqB0nsbDx7J4&ust=1543808840854112",
-        scores: [1, 2, 3, 4, 5, 6, 7, 8]
+        scores: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     },
     {
         name: "Donald",
         photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjugfy5noDfAhVGeawKHWD7AzYQjRx6BAgBEAU&url=https%3A%2F%2Fwww.forbes.com%2Fprofile%2Fdonald-glover%2F&psig=AOvVaw2IYby8AkmkEqB0nsbDx7J4&ust=1543808840854112",
-        scores: [1, 2, 3, 4, 5, 6, 7, 8]
+        scores: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     },
     {
         name: "Donald",
         photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjugfy5noDfAhVGeawKHWD7AzYQjRx6BAgBEAU&url=https%3A%2F%2Fwww.forbes.com%2Fprofile%2Fdonald-glover%2F&psig=AOvVaw2IYby8AkmkEqB0nsbDx7J4&ust=1543808840854112",
-        scores: [1, 2, 3, 4, 5, 6, 7, 8]
+        scores: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     },
     {
         name: "Donald",
         photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjugfy5noDfAhVGeawKHWD7AzYQjRx6BAgBEAU&url=https%3A%2F%2Fwww.forbes.com%2Fprofile%2Fdonald-glover%2F&psig=AOvVaw2IYby8AkmkEqB0nsbDx7J4&ust=1543808840854112",
-        scores: [1, 2, 3, 4, 5, 6, 7, 8]
+        scores: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     }]   
---
+++ server.js
@@ -0,0 +1,33 @@
+// require dependencies  
+var http = require("http");
+var fs = require("fs");
+
+// set port to 8080
+var PORT = 8080;
+
+var server = http.createServer(handleRequest);
+
+function handleRequest(req, res) {
+
+    // capture the url to which the request is made
+    var path = req.url;
+
+    // when we visit different urls, read and respond with different files
+    switch (path) {
+    case "/index":
+      return fs.readFile(__dirname + "/app/public/home.html", function(err, data) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      });
+    case "/survey":
+      return fs.readFile(__dirname + "/app/public/survey.html", function(err, data) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      });
+    }
+}
+
+// starts the server
+server.listen(PORT, function() {
+    console.log("Server is listening on PORT: " + PORT);
+});
